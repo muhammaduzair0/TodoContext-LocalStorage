9a4dcaee6483c456eeec563a2924da3dac4b1baa
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-14 12:57:37 +0500

    Added State of todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 function App() {
+  const [todos, setTodos] = useState([]);
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
