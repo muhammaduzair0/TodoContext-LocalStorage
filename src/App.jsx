2baf4bf153ad21f3e92f8b19020df61e54fb7160
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-14 15:13:05 +0500

    Toggle Complete Functionality Added

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,16 @@ function App() {
   const deleteTodo = (id) => {
     setTodos((prev) => prev.filter((todo) => todo.id !== id));
   };
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
   return (
     <TodoProvider
       value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
