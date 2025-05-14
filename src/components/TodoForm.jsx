75af13980ccf2d20d8ee3c3da467594ae632c055
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-14 18:13:01 +0500

    Add Todo Msg In Todo Form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -5,6 +5,12 @@ const TodoForm = () => {
   const [todo, setTodo] = useState();
   const { addTodo } = useTodo();
 
+  const add = (e) => {
+    e.preventDefault();
+    if (!todo) return;
+    addTodo({ todo, completed: false });
+    setTodo("");
+  };
   return (
     <form onSubmit={add} className="flex">
       <input
