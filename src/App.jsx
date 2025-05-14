0fb42de6c5cd26f87eae476e90904135da0eec8f
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-14 12:58:57 +0500

    Added Functionality of upadate Todo

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ function App() {
   const addTodo = (todo) => {
     setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
   };
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+  return (
     <TodoProvider
       value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
     >
