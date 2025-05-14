3ef14cecd54abc5a3d994f662d3f1eeae7a291f2
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-14 15:12:49 +0500

    Delete Todo Functionality Added

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ function App() {
       prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
     );
   };
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
   return (
     <TodoProvider
       value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
