b597f2c84ba880350fb7a1c479e66fe6058eeaf7
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-14 18:10:58 +0500

    Edit Todo Functionality Added

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -6,6 +6,12 @@ const TodoItem = ({ todo }) => {
   const [todoMsg, setTodoMsg] = useState(todo.todo);
 
   const { updateTodo, deleteTodo, toggleComplete } = useTodo();
+
+  const editTodo = () => {
+    updateTodo(todo.id, { ...todo, todo: todoMsg });
+    setIsTodoEditable(false);
+  };
+
   return (
     <div
       className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
